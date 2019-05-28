077e8be7d88af24a8ba9eddf8c3dc447cc8a15de
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-05-28 16:14:08 -0300

    Ajustando o endpoint

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const HOST = 'https://beta.greenmile.com/';
 
-export const LOGIN_ENDPOINT = 'Login';
+export const LOGIN_ENDPOINT = 'login';
 export const ORGANIZATIONS_ENDPOINT = 'Organization';
