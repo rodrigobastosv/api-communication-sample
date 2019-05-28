b001742df72ba9532dc9826827c9662f6dcb9fea
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-05-28 14:49:51 -0300

    Adicionando toolbar

--- src/components/organization/OrganizationList.js
+++ src/components/organization/OrganizationList.js
@@ -1,5 +1,10 @@
 import React, { PureComponent, Fragment } from "react";
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 import { OrganizationCard } from './organizationCard/OrganizationCard';
 
 export class OrganizationList extends PureComponent {
@@ -7,6 +12,14 @@ export class OrganizationList extends PureComponent {
     render() {
         return (
             <Fragment>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="Menu">
+                            <MenuIcon />
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+
                 <OrganizationCard></OrganizationCard>
                 <OrganizationCard></OrganizationCard>
                 <OrganizationCard></OrganizationCard>
