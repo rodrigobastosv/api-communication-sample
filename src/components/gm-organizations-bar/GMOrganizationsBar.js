a3ac9a4e5a2a8826ac8d5ac5e04654c896c29134
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-05-29 14:28:16 -0300

    Adicionando menu fancy

--- src/components/gm-organizations-bar/GMOrganizationsBar.js
+++ src/components/gm-organizations-bar/GMOrganizationsBar.js
@@ -5,6 +5,12 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import ExitToAppRounded from '@material-ui/icons/ExitToAppRounded';
 
 import classes from './GMOrganizationsBar.module.css';
@@ -14,18 +20,31 @@ export class GMOrganizationsBar extends PureComponent {
     render() {
         return (
             <Fragment>
-                <AppBar position="fixed">
+                <AppBar className={classes.appbar} position="fixed">
                     <Toolbar className={classes.toolbar}>
-                        <IconButton edge="start" color="inherit" aria-label="Menu">
-                            <MenuIcon />
-                        </IconButton>
+                        <Avatar className={classes.greenmileAvatar}>GM</Avatar>
+
                         <Typography variant="h5" component="h2">
                             Organizations
                         </Typography>
 
-                        <IconButton onClick={() => this.props.history.goBack()} aria-label="Delete">
-                            <ExitToAppRounded />
-                        </IconButton>
+                        <PopupState variant="popper" popupId="demo-popup-menu">
+                            {popupState => (
+                                <React.Fragment>
+                                    <IconButton edge="start" color="inherit" aria-label="Menu" {...bindTrigger(popupState)}>
+                                        <MenuIcon />
+                                    </IconButton>
+                                    <Menu {...bindMenu(popupState)}>
+                                        <MenuItem onClick={() => this.props.history.goBack()}>
+                                            <ListItemIcon>
+                                                <ExitToAppRounded />
+                                            </ListItemIcon>
+                                            <ListItemText primary="Logout" />
+                                        </MenuItem>
+                                    </Menu>
+                                </React.Fragment>
+                            )}
+                        </PopupState>
                     </Toolbar>
                 </AppBar>
             </Fragment>
